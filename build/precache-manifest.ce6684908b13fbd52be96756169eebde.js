ab14c8e1cbd2db8ea01c670453390eaf4380a7b7
Timo <timo_moilanen@hotmail.com>
2020-06-03 19:37:25 +0300

    uibuild

---
+++ build/precache-manifest.ce6684908b13fbd52be96756169eebde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b071868a0c39bc34998b721abc1da93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554a791ba3476097f00e",
+    "url": "/static/css/main.5fb36046.chunk.css"
+  },
+  {
+    "revision": "27bd1cdff6215b46af31",
+    "url": "/static/js/2.fedff18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fedff18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554a791ba3476097f00e",
+    "url": "/static/js/main.8d5f7dc8.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
